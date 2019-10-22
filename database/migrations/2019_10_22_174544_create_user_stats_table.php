8b90395cfe34bf1d72259d9fd546b9d403a0498e
Laurentiu <dodoloilaurentiu@gmail.com>
2019-10-22 20:47:16 +0300

    Create models, controllers and migrations.

---
+++ database/migrations/2019_10_22_174544_create_user_stats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_stats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_stats');
+    }
+}
